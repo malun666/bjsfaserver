eef387972efee821a25fa02e25b6ab76bb7776a6
malun <malun666@126.com>
2018-09-08 11:33:50 +0800

    add the log4js

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const Koa = require('koa');
 const app = new Koa();
 const serverStatic = require('koa-static2')
 
+const logger = require('koa-logger');
+const loggerWriter = require('./common/log');
+
+app.use(logger((str, args) => {
+  // loggerWriter.debug(`${args[1]} ${args[2]} 状态码：${args[3]} 用时：${args[4]} 数据大小: ${args[5]}`);
+}));
+
 // 设置静态目录
 app.use(serverStatic('/public', path.join(__dirname, 'public')))
 
---
+++ common/log.js
@@ -0,0 +1,14 @@
+const log4js = require('log4js');
+log4js.configure({
+  appenders:{
+    file: {
+      type: 'dateFile',
+      pattern: 'yyyy_MM_dd',
+      filename: 'app.log',
+      keepFileExt: true
+    }
+  },
+  categories: { default: {appenders: ['file'], level: 'debug'}}
+});
+
+module.exports = log4js.getLogger('app');
