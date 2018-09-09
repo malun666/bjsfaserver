489b190ae877425457a5bc7dac4e7d50397d1a1f
malun <malun666@126.com>
2018-09-09 17:25:14 +0800

    add

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,6 +2,6 @@ module.exports = {
   getUser(ctx) {
     console.log(ctx.params);
     ctx.response.type = 'json';
-    ctx.response.body = [1, 2, {age: 19, name: ctx.params.id}];
+    ctx.response.body = [{age: 19, name: ctx.params.id}];
   }
 }
