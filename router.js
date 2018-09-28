7093efc67af61e67769901564380ee43d7447b06
malun <malun666@126.com>
2018-09-28 18:25:25 +0800

    update the router.js

--- router.js
+++ router.js
@@ -1,27 +1,15 @@
 const controllers = require('./controllers/index');
-// const router = require('koa-router')();
 const Router = require('koa-router');
 
 let router = new Router({
-  prefix: '/api'
+  prefix: '/api'  // 设置所有的路由的前缀
 });
 
-router.get('/', (ctx) => {
-  ctx.body = 'hi, koa';
-});
-
+// 自定义的路由规则
 router.post('/login', controllers.Login.auth)
 router.post('/login/init', controllers.Init.init)
-
-router.get('/user', controllers.User.getUser);
-// router.get('/user/:id', controllers.User.getUserById);
-// router.delete('/user/:id', controllers.User.deleteUserById);
-// router.post('/user', controllers.User.postUser);
-// router.put('/user/:id', controllers.User.updateUser);
-// router.patch('/user/:id', controllers.User.updateUser);
-
 router.post('/uploadFile', controllers.File.postUploadFile);
-
+// 设置自动生成的路由
 require('./router_auto')(router);
 
 module.exports = router;
